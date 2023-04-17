fed18bc5ff1571d415229708f18459064ddd7e9b
haibane_tenshi <obscure.sanctum@gmail.com>
2023-04-17 13:39:58 +0300

    feat(OpCode): add LoadConstant opcode

--- src/opcode.rs
+++ src/opcode.rs
@@ -1,19 +1,38 @@
+use crate::value::Value;
 use std::fmt::Display;
+
+pub type Index = u32;
 
 #[derive(Debug, Copy, Clone)]
 pub enum OpCode {
     Return,
+    LoadConstant(Index),
 }
 
 impl Display for OpCode {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        std::fmt::Debug::fmt(self, f)
+        use OpCode::*;
+
+        let s = match *self {
+            Return => "Return",
+            LoadConstant(_) => "LoadConst",
+        };
+
+        write!(f, "{s}")
     }
 }
 
 #[derive(Debug, Clone)]
 pub struct Chunk {
     pub codes: Vec<OpCode>,
+    pub constants: Vec<Value>,
+}
+
+impl Chunk {
+    pub fn get_constant(&self, index: Index) -> Option<&Value> {
+        let index: usize = index.try_into().ok()?;
+        self.constants.get(index)
+    }
 }
 
 impl Display for Chunk {
@@ -21,7 +40,19 @@ impl Display for Chunk {
         writeln!(f, "== chunk ==")?;
 
         for (i, code) in self.codes.iter().copied().enumerate() {
-            writeln!(f, "{i:04} {code}")?;
+            use OpCode::*;
+
+            match code {
+                Return => writeln!(f, "{i:04} {code:<9}")?,
+                LoadConstant(index) => {
+                    let constant = self
+                        .get_constant(index)
+                        .map(|t| t.to_string())
+                        .unwrap_or_else(|| "<FAILED TO RESOLVE CONSTANT>".to_string());
+
+                    writeln!(f, "{i:04} {code:<9} {index:03} {constant}")?;
+                }
+            }
         }
 
         Ok(())
--- src/value.rs
+++ src/value.rs
@@ -1,3 +1,5 @@
+use std::fmt::Display;
+
 #[derive(Debug, Copy, Clone, PartialEq)]
 pub enum Value {
     Nil,
@@ -9,5 +11,18 @@ pub enum Value {
 impl Value {
     pub fn is_falsey(self) -> bool {
         matches!(self, Value::Nil | Value::Bool(false))
+    }
+}
+
+impl Display for Value {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        use Value::*;
+
+        match *self {
+            Nil => write!(f, "nil"),
+            Bool(v) => write!(f, "{v}"),
+            Uint(v) => write!(f, "{v}_u64"),
+            Float(v) => write!(f, "{v}_f64"),
+        }
     }
 }
