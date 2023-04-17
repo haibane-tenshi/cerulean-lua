47d44dc8fa52dff82eded28a599c23356f55051b
haibane_tenshi <obscure.sanctum@gmail.com>
2023-04-17 14:12:35 +0300

    feat(Chunk): add line numbers

--- src/opcode.rs
+++ src/opcode.rs
@@ -1,5 +1,8 @@
-use crate::value::Value;
 use std::fmt::Display;
+
+use rle_vec::RleVec;
+
+use crate::value::Value;
 
 pub type Index = u32;
 
@@ -26,6 +29,7 @@ impl Display for OpCode {
 pub struct Chunk {
     pub codes: Vec<OpCode>,
     pub constants: Vec<Value>,
+    pub lines: RleVec<u32>,
 }
 
 impl Chunk {
@@ -39,18 +43,43 @@ impl Display for Chunk {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         writeln!(f, "== chunk ==")?;
 
-        for (i, code) in self.codes.iter().copied().enumerate() {
+        #[derive(Copy, Clone)]
+        enum LineNumber {
+            Explicit(u32),
+            Repeat,
+        }
+
+        let iter = self.codes.iter().copied().enumerate().zip(
+            self.lines
+                .runs()
+                .flat_map(|run| {
+                    std::iter::once(LineNumber::Explicit(*run.value))
+                        .chain(std::iter::repeat(LineNumber::Repeat).take(run.len - 1))
+                })
+                .map(Some)
+                .chain(std::iter::repeat(None)),
+        );
+
+        for ((i, code), line) in iter {
             use OpCode::*;
 
+            let line = match line {
+                Some(LineNumber::Explicit(n)) => n.to_string(),
+                Some(LineNumber::Repeat) => "|".to_string(),
+                None => "?".to_string(),
+            };
+
+            write!(f, "{i:04} {line:>3} {code:<9}")?;
+
             match code {
-                Return => writeln!(f, "{i:04} {code:<9}")?,
+                Return => (),
                 LoadConstant(index) => {
                     let constant = self
                         .get_constant(index)
                         .map(|t| t.to_string())
                         .unwrap_or_else(|| "<FAILED TO RESOLVE CONSTANT>".to_string());
 
-                    writeln!(f, "{i:04} {code:<9} {index:03} {constant}")?;
+                    writeln!(f, " {index:03} {constant}")?;
                 }
             }
         }
