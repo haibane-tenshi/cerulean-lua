3aee9973ccd76c04fd22b2ea9e7ea7ff64eedea6
haibane_tenshi <obscure.sanctum@gmail.com>
2023-04-18 10:52:35 +0300

    feat(parser): implement simple local variable assignment

--- src/parser.rs
+++ src/parser.rs
@@ -1,7 +1,7 @@
 use std::collections::HashMap;
 
 use crate::lex::Token;
-use crate::opcode::{Chunk, ConstId};
+use crate::opcode::{Chunk, ConstId, OpCode};
 use crate::value::Literal;
 
 pub struct Error;
@@ -13,10 +13,6 @@ struct ConstStorage {
 }
 
 impl ConstStorage {
-    pub fn new() -> Self {
-        Default::default()
-    }
-
     pub fn insert(&mut self, value: Literal) -> ConstId {
         *self.backlinks.entry(value).or_insert_with(|| {
             let index = self.constants.len().try_into().unwrap();
@@ -31,13 +27,51 @@ impl ConstStorage {
     }
 }
 
+#[derive(Debug, Default)]
+struct Storages {
+    constants: ConstStorage,
+    codes: Vec<OpCode>,
+}
+
 fn next<'a, 's>(s: &'a [Token<'s>]) -> Result<(Token<'s>, &'a [Token<'s>]), Error> {
     let (token, s) = s.split_first().ok_or(Error)?;
     Ok((*token, s))
 }
 
-pub fn chunk(s: &[Token]) -> Result<Chunk, Error> {
-    todo!()
+pub fn chunk(mut s: &[Token]) -> Result<Chunk, Error> {
+    let mut storages = Storages::default();
+
+    while !s.is_empty() {
+        (s, _) = assignment(s, &mut storages)?;
+    }
+
+    let Storages { codes, constants } = storages;
+
+    let constants = constants.resolve();
+
+    let chunk = Chunk {
+        codes,
+        constants,
+        lines: Default::default(),
+    };
+
+    Ok(chunk)
+}
+
+fn assignment<'a, 's>(
+    s: &'a [Token<'s>],
+    storages: &mut Storages,
+) -> Result<(&'a [Token<'s>], ()), Error> {
+    let (_, s) = match s {
+        [Token::Local, Token::Ident(ident), Token::Assign, rest @ ..] => (ident, rest),
+        _ => return Err(Error),
+    };
+
+    let (s, const_id) = literal(s, &mut storages.constants)?;
+
+    storages.codes.push(OpCode::LoadConstant(const_id));
+
+    Ok((s, ()))
 }
 
 fn literal<'a, 's>(
