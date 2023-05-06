cb2409aa1dde62194798b58e938ea220a66ae454
haibane_tenshi <obscure.sanctum@gmail.com>
2023-05-06 15:29:49 +0300

    feat(parser): implement multi-assignments

--- src/parser/stmt/assignment.rs
+++ src/parser/stmt/assignment.rs
@@ -1,18 +1,70 @@
+use crate::parser::prefix_expr::Place;
 use crate::parser::prelude::*;
 
 pub(super) fn assignment<'s>(
     s: Lexer<'s>,
     tracker: &mut ChunkTracker<'s>,
 ) -> Result<(Lexer<'s>, ()), LexParseError> {
-    use crate::parser::{expr_adjusted_to_1, identifier, match_token};
+    use crate::parser::{expr_list_adjusted_to, match_token};
 
-    let (s, ident) = identifier(s)?;
+    let outer = tracker.current_mut()?.start_block()?;
+
+    let mut places_start = tracker.current()?.stack_top()?;
+    let (s, places) = places(s, tracker)?;
+    let count = places.len().try_into().unwrap();
+
     let (s, ()) = match_token(s, Token::Assign).require()?;
-    let (s, ()) = expr_adjusted_to_1(s, tracker).require()?;
 
-    // Try to store it inside known variable.
-    let slot = tracker.lookup_local(ident).ok_or(ParseError)?;
-    tracker.current_mut()?.emit(OpCode::StoreStack(slot))?;
+    let expr_start = tracker.current()?.stack_top()?;
+    let (s, ()) = expr_list_adjusted_to(s, tracker, count).require()?;
+
+    let current = tracker.current_mut()?;
+
+    let expr_slots = (expr_start.0..).map(StackSlot);
+    for (expr_slot, place) in expr_slots.zip(places) {
+        match place {
+            Place::Temporary(slot) => {
+                current.emit(OpCode::LoadStack(expr_slot))?;
+                current.emit(OpCode::StoreStack(slot))?;
+            }
+            Place::TableField => {
+                let table = places_start;
+                let field = places_start + 1;
+                places_start += 2;
+
+                current.emit(OpCode::LoadStack(table))?;
+                current.emit(OpCode::LoadStack(field))?;
+                current.emit(OpCode::LoadStack(expr_slot))?;
+                current.emit(OpCode::TabSet)?;
+            }
+        }
+    }
+
+    current.finish_block(outer)?;
 
     Ok((s, ()))
+}
+
+fn places<'s>(
+    s: Lexer<'s>,
+    tracker: &mut ChunkTracker<'s>,
+) -> Result<(Lexer<'s>, Vec<Place>), LexParseError> {
+    use crate::parser::match_token;
+    use crate::parser::prefix_expr::place;
+
+    let (mut s, first) = place(s, tracker)?;
+
+    let mut r = vec![first];
+
+    let mut next_part = |s: Lexer<'s>| -> Result<(Lexer<'s>, Place), LexParseError> {
+        let (s, ()) = match_token(s, Token::Comma)?;
+        place(s, tracker).require()
+    };
+
+    while let Ok((ns, next)) = next_part(s.clone()) {
+        s = ns;
+        r.push(next);
+    }
+
+    Ok((s, r))
 }
