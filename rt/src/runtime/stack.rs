3f3723bdaa8f8d18b39f6c008b3dc6872f24a5d7
haibane_tenshi <obscure.sanctum@gmail.com>
2023-11-27 20:26:05 +0300

    feat(rt::runtime::stack): add few convenience methods to StackView

--- rt/src/runtime/stack.rs
+++ rt/src/runtime/stack.rs
@@ -1,8 +1,9 @@
 use std::collections::HashMap;
 use std::fmt::Debug;
-use std::ops::{Add, Range};
+use std::ops::{Add, Range, RangeBounds};
 
 use repr::index::StackSlot;
+use repr::tivec::TiVec;
 
 use super::Value;
 use crate::RuntimeError;
@@ -38,6 +39,18 @@ impl Add<StackSlot> for RawStackSlot {
     }
 }
 
+impl From<usize> for RawStackSlot {
+    fn from(value: usize) -> Self {
+        RawStackSlot(value)
+    }
+}
+
+impl From<RawStackSlot> for usize {
+    fn from(value: RawStackSlot) -> Self {
+        value.0
+    }
+}
+
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Default, Hash)]
 pub struct RawUpvalueId(u32);
 
@@ -53,25 +66,29 @@ impl RawUpvalueId {
 pub struct UpvalueId(RawUpvalueId, RawStackSlot);
 
 pub struct Stack<C> {
-    temporaries: Vec<Value<C>>,
+    temporaries: TiVec<RawStackSlot, Value<C>>,
     on_stack_upvalues: HashMap<RawStackSlot, RawUpvalueId>,
     evicted_upvalues: HashMap<RawUpvalueId, Value<C>>,
     next_upvalue_id: RawUpvalueId,
 }
 
 impl<C> Stack<C> {
-    // pub fn new() -> Self {
-    //     Default::default()
-    // }
+    pub fn new() -> Self {
+        Default::default()
+    }
 
-    pub(crate) fn push(&mut self, value: Value<C>) -> RawStackSlot {
+    pub fn view(&mut self) -> StackView<C> {
+        StackView::new(self)
+    }
+
+    fn push(&mut self, value: Value<C>) -> RawStackSlot {
         let slot = self.temporaries.len();
         self.temporaries.push(value);
 
         RawStackSlot(slot)
     }
 
-    pub(crate) fn pop(&mut self) -> Option<Value<C>> {
+    fn pop(&mut self) -> Option<Value<C>> {
         let r = self.temporaries.pop();
         if let Some(value) = &r {
             let slot = RawStackSlot(self.temporaries.len());
@@ -83,7 +100,7 @@ impl<C> Stack<C> {
         r
     }
 
-    pub(crate) fn adjust_height_with_variadics(&mut self, height: RawStackSlot) -> Vec<Value<C>> {
+    fn adjust_height_with_variadics(&mut self, height: RawStackSlot) -> Vec<Value<C>> {
         match height.0.checked_sub(self.temporaries.len()) {
             None => {
                 for slot in (height.0..self.len().0).map(RawStackSlot) {
@@ -93,7 +110,7 @@ impl<C> Stack<C> {
                     }
                 }
 
-                self.temporaries.split_off(height.0)
+                self.temporaries.split_off(height).into()
             }
             Some(0) => Default::default(),
             Some(n) => {
@@ -104,34 +121,31 @@ impl<C> Stack<C> {
         }
     }
 
-    pub(crate) fn drop_range(&mut self, range: Range<RawStackSlot>) -> Result<(), RuntimeError> {
-        let range = range.start.0..range.end.0;
-        for (value, slot) in self
-            .temporaries
-            .drain(range.clone())
-            .zip(range.map(RawStackSlot))
-        {
-            if let Some(upvalue_id) = self.on_stack_upvalues.remove(&slot) {
-                self.evicted_upvalues.insert(upvalue_id, value);
-            }
-        }
+    fn remove_range(&mut self, range: Range<RawStackSlot>) -> Result<(), RuntimeError> {
+        self.evict_upvalues(range.clone());
+        self.temporaries.drain(range);
 
         Ok(())
     }
 
-    pub(crate) fn get(&self, slot: RawStackSlot) -> Option<&Value<C>> {
-        self.temporaries.get(slot.0)
+    fn truncate(&mut self, new_len: RawStackSlot) {
+        self.evict_upvalues(new_len..);
+        self.temporaries.truncate(new_len.0);
     }
 
-    pub(crate) fn get_mut(&mut self, slot: RawStackSlot) -> Option<&mut Value<C>> {
-        self.temporaries.get_mut(slot.0)
+    fn get(&self, slot: RawStackSlot) -> Option<&Value<C>> {
+        self.temporaries.get(slot)
     }
 
-    pub fn last(&self) -> Option<&Value<C>> {
+    fn get_mut(&mut self, slot: RawStackSlot) -> Option<&mut Value<C>> {
+        self.temporaries.get_mut(slot)
+    }
+
+    fn last(&self) -> Option<&Value<C>> {
         self.temporaries.last()
     }
 
-    pub fn fresh_upvalue(&mut self, value: Value<C>) -> UpvalueId {
+    fn fresh_upvalue(&mut self, value: Value<C>) -> UpvalueId {
         let raw_id = self.next_upvalue_id.increment();
 
         self.evicted_upvalues.insert(raw_id, value);
@@ -144,19 +158,19 @@ impl<C> Stack<C> {
         UpvalueId(raw_id, RawStackSlot(0))
     }
 
-    pub fn get_upvalue(&self, upvalue: UpvalueId) -> Option<&Value<C>> {
+    fn get_upvalue(&self, upvalue: UpvalueId) -> Option<&Value<C>> {
         self.evicted_upvalues
             .get(&upvalue.0)
             .or_else(|| self.get(upvalue.1))
     }
 
-    pub fn get_upvalue_mut(&mut self, upvalue: UpvalueId) -> Option<&mut Value<C>> {
+    fn get_upvalue_mut(&mut self, upvalue: UpvalueId) -> Option<&mut Value<C>> {
         self.evicted_upvalues
             .get_mut(&upvalue.0)
-            .or_else(|| self.temporaries.get_mut(upvalue.1 .0))
+            .or_else(|| self.temporaries.get_mut(upvalue.1))
     }
 
-    pub(crate) fn mark_as_upvalue(&mut self, slot: RawStackSlot) -> Option<UpvalueId> {
+    fn mark_as_upvalue(&mut self, slot: RawStackSlot) -> Option<UpvalueId> {
         if slot.0 >= self.len().0 {
             return None;
         }
@@ -169,9 +183,47 @@ impl<C> Stack<C> {
         Some(UpvalueId(upvalue_id, slot))
     }
 
-    pub(crate) fn len(&self) -> RawStackSlot {
+    fn evict_upvalue(&mut self, slot: RawStackSlot) {
+        let Some(upvalue_id) = self.on_stack_upvalues.remove(&slot) else {
+            return;
+        };
+
+        let value = self
+            .temporaries
+            .get_mut(slot)
+            .expect("there should not exist upvalue id for empty stack slot");
+
+        self.evicted_upvalues.insert(upvalue_id, value.take());
+    }
+
+    fn evict_upvalues(&mut self, range: impl RangeBounds<RawStackSlot>) {
+        use std::ops::Bound;
+
+        let start = match range.start_bound() {
+            Bound::Included(t) => t.0,
+            Bound::Excluded(t) => t.0 + 1,
+            Bound::Unbounded => 0,
+        };
+
+        let end = match range.end_bound() {
+            Bound::Included(t) => t.0 - 1,
+            Bound::Excluded(t) => t.0,
+            Bound::Unbounded => self.temporaries.len(),
+        };
+
+        for slot in (start..end).map(RawStackSlot) {
+            self.evict_upvalue(slot)
+        }
+    }
+
+    fn len(&self) -> RawStackSlot {
         RawStackSlot(self.temporaries.len())
     }
+
+    // fn clear(&mut self) {
+    //     self.evict_upvalues(..);
+    //     self.temporaries.clear();
+    // }
 }
 
 impl<C> Debug for Stack<C> {
@@ -202,7 +254,7 @@ pub struct StackView<'a, C> {
 }
 
 impl<'a, C> StackView<'a, C> {
-    pub(crate) fn new(stack: &'a mut Stack<C>) -> Self {
+    pub fn new(stack: &'a mut Stack<C>) -> Self {
         StackView {
             stack,
             protected_size: RawStackSlot(0),
@@ -285,9 +337,18 @@ impl<'a, C> StackView<'a, C> {
 
     pub fn drop_under(&mut self, slot: StackSlot) -> Result<(), RuntimeError> {
         let height = self.protected_size.index(slot);
-        self.stack.drop_range(self.protected_size..height)?;
+        self.stack.remove_range(self.protected_size..height)?;
 
         Ok(())
+    }
+
+    pub fn clear(&mut self) {
+        self.truncate(StackSlot(0))
+    }
+
+    pub fn truncate(&mut self, new_len: StackSlot) {
+        let new_len = self.protected_size.index(new_len);
+        self.stack.truncate(new_len)
     }
 }
 
