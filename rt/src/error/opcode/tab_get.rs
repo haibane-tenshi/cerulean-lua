2e540d4b0217a6303324ae92b6fc676c48f310fe
haibane_tenshi <obscure.sanctum@gmail.com>
2023-12-05 03:12:38 +0300

    refactor(rt::error::opcode::TabGet): rework diagnostic generation

--- rt/src/error/opcode/tab_get.rs
+++ rt/src/error/opcode/tab_get.rs
@@ -49,6 +49,7 @@ pub enum RuntimeCause {
 }
 
 impl RuntimeCause {
+    #[allow(clippy::single_match)]
     fn into_diagnostic<FileId>(
         self,
         file_id: FileId,
@@ -60,80 +61,98 @@ impl RuntimeCause {
         use RuntimeCause::*;
         use TabGet::*;
 
-        let mut diag = Diagnostic::error();
+        let diag = Diagnostic::error();
 
-        let mut diag = match self {
-            TableTypeMismatch(_) => diag.with_message("only tables can be indexed"),
-            InvalidKey(err) => {
-                match err {
-                    InvalidTableKeyError::Nan => diag.with_note([
-                        "Lua does not permit indexing tables using float NaN (not a number)",
-                        "Lua follows floating point standard which mandates that no two NaN values are equal even if their bitwise representation is identical\ntherefore, values under NaN keys would be impossible to look up",
-                    ]),
-                    InvalidTableKeyError::Nil => diag.with_note(["Lua does not permit indexing tables using `nil`"]),
-                };
+        // Message
+        let mut diag = match (self, &debug_info) {
+            (TableTypeMismatch(ty), Some(GlobalEnv { .. })) => diag.with_message(format!(
+                "expected `_ENV` to be table but it has type `{}`",
+                ty.to_lua_name()
+            )),
+            (TableTypeMismatch(_), _) => diag.with_message("only tables can be indexed"),
+            (InvalidKey(_), _) => diag.with_message("invalid table key"),
+        };
 
-                diag.with_message("invalid table key")
+        // Labels
+        match (self, debug_info.clone()) {
+            (
+                TableTypeMismatch(ty),
+                Some(Local {
+                    table,
+                    index: _,
+                    indexing,
+                }),
+            ) => {
+                diag.with_label([
+                    Label::primary(file_id.clone(), table).with_message(format!(
+                        "expected table, but this has type `{}`",
+                        ty.to_lua_name()
+                    )),
+                    Label::secondary(file_id.clone(), indexing)
+                        .with_message("table indexing happens here"),
+                ]);
+            }
+            (TableTypeMismatch(_), Some(GlobalEnv { ident })) => {
+                diag.with_label([Label::secondary(file_id.clone(), ident)
+                    .with_message("identifier does not refer to local variable")]);
+            }
+            (
+                InvalidKey(err),
+                Some(Local {
+                    table: _,
+                    index,
+                    indexing,
+                }),
+            ) => {
+                diag.with_label([
+                    Label::primary(file_id.clone(), index)
+                        .with_message(format!("index has value `{}`", err.value_str())),
+                    Label::secondary(file_id.clone(), indexing)
+                        .with_message("table indexing happens here"),
+                ]);
+            }
+            (InvalidKey(err), Some(GlobalEnv { ident })) => {
+                diag.with_label([Label::primary(file_id.clone(), ident)
+                    .with_message(format!("index has value `{}`", err.value_str()))]);
+            }
+            (_, None) => (),
+        }
+
+        // General notes
+        match &debug_info {
+            Some(GlobalEnv { .. }) => {
+                diag.with_note([
+                    "Lua implicitly accesses `_ENV` table to look up non-local variables",
+                ]);
+            }
+            _ => (),
+        }
+
+        match self {
+            TableTypeMismatch(_) => (),
+            InvalidKey(err) => match err {
+                InvalidTableKeyError::Nan => diag.with_note([
+                    "Lua does not permit indexing tables using float NaN (not a number)",
+                    "Lua follows floating point standard which mandates that no two NaN values are equal even if their bitwise representation is identical\ntherefore, values under NaN keys would be impossible to look up",
+                ]),
+                InvalidTableKeyError::Nil => diag.with_note(["Lua does not permit indexing tables using `nil`"]),
             }
         };
 
-        match debug_info {
-            Some(Local {
-                table,
-                index,
-                indexing,
-            }) => match self {
-                TableTypeMismatch(ty) => {
-                    diag.with_label([
-                        Label::primary(file_id.clone(), table).with_message(format!(
-                            "expected table, but this has type `{}`",
-                            ty.to_lua_name()
-                        )),
-                        Label::secondary(file_id.clone(), indexing)
-                            .with_message("indexing happens here"),
-                    ]);
-                }
-                InvalidKey(err) => {
-                    diag.with_label([
-                        Label::primary(file_id.clone(), index)
-                            .with_message(format!("index has value `{}`", err.value_str())),
-                        Label::secondary(file_id.clone(), indexing)
-                            .with_message("table indexing happens here"),
-                    ]);
-                }
-            },
-            Some(GlobalEnv { ident }) => match self {
-                TableTypeMismatch(ty) => {
-                    diag.with_label([Label::secondary(file_id.clone(), ident)
-                        .with_message("identifier does not refer to local variable")]);
-
-                    diag.with_note([format!(
-                        "expected `_ENV` to be table but it has type `{}`",
-                        ty.to_lua_name()
-                    )]);
-                    diag.with_note([
-                        "Lua implicitly accesses `_ENV` table to look up non-local variables",
-                    ]);
-                    diag.with_help([
-                        "perhaps you forgot to initialize global env when creating runtime?",
-                        "perhaps you assigned to `_ENV` somewhere?\nconsider that `_ENV` is a regular variable, so it is possible to assign to or shadow it",
-                        "perhaps script received misconfigured global env?\nby default scripts recieve runtime's global env on call, but it is possible to override this behavior\nsee documentation for Lua std `load` function",
-                    ]);
-                }
-                InvalidKey(_) => {
-                    diag.with_label([
-                        Label::primary(file_id.clone(), ident).with_message("index value")
-                    ]);
-
-                    diag.with_note([
-                        "Lua implicitly accesses `_ENV` table to look up non-local variables",
-                    ]);
-                }
-            },
-            None => {
+        // Special notes
+        match (self, &debug_info) {
+            (TableTypeMismatch(_), Some(GlobalEnv { .. })) => {
+                diag.with_help([
+                    "perhaps you forgot to initialize global env when creating runtime?",
+                    "perhaps you assigned to `_ENV` somewhere?\nconsider that `_ENV` is a regular variable, so it is possible to assign to or shadow it",
+                    "perhaps script received misconfigured global env?\nby default scripts recieve runtime's global env on call, but it is possible to override this behavior\nsee documentation for Lua std `load` function",
+                ]);
+            }
+            (_, None) => {
                 diag.with_note(["no debug info is available"]);
             }
-        };
+            _ => (),
+        }
 
         diag
     }
