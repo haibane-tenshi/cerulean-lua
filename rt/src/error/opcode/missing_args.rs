ffdffe76a4cf360ce1a57401f23102353da3d4e6
haibane_tenshi <obscure.sanctum@gmail.com>
2023-12-07 03:58:25 +0300

    refactor(rt::error::opcode::MissingArgsError): move to its own module

---
+++ rt/src/error/opcode/missing_args.rs
@@ -0,0 +1,50 @@
+use codespan_reporting::diagnostic::Diagnostic;
+use repr::opcode::OpCode;
+use std::ops::Range;
+
+#[derive(Debug, Clone)]
+pub struct MissingArgsError {
+    pub expected_args: usize,
+    pub stack_len: usize,
+}
+
+impl MissingArgsError {
+    pub(super) fn into_diagnostic<FileId>(
+        self,
+        file_id: FileId,
+        opcode: OpCode,
+        debug_info: Option<Range<usize>>,
+    ) -> Diagnostic<FileId> {
+        use super::{compiler_bug, ExtraDiagnostic};
+        use codespan_reporting::diagnostic::Label;
+
+        let MissingArgsError {
+            expected_args,
+            stack_len,
+        } = self;
+
+        let name = opcode.name();
+        let have_debug_info = debug_info.is_some();
+        let mut diag = Diagnostic::bug()
+            .with_message(format!("bytecode instruction {name} expects {expected_args} argument(s) but only {stack_len} were provided"));
+
+        if let Some(span) = debug_info {
+            diag.with_label([Label::secondary(file_id, span)
+                .with_message("error occurred here, but its cause in different place")]);
+        }
+
+        diag.with_note(compiler_bug([
+            "the opcode gets its arguments from stack but it seems there is not enough values to take",
+            "the error implies that too few values were put on stack however it could have happenned long before this point",
+        ]));
+
+        if !have_debug_info {
+            diag.with_note([
+                "no debug info is available",
+                "it is possible debug info was stripped\nit is also possible that erroneous bytecode was handcrafted\nplease check with where you got it",
+            ]);
+        }
+
+        diag
+    }
+}
--- rt/src/error/opcode/mod.rs
+++ rt/src/error/opcode/mod.rs
@@ -1,4 +1,5 @@
 mod bin_op;
+mod missing_args;
 mod table;
 mod una_op;
 
@@ -8,6 +9,7 @@ use repr::opcode::OpCode;
 use std::ops::Range;
 
 pub use bin_op::Cause as BinOpCause;
+pub use missing_args::MissingArgsError;
 pub use table::RuntimeCause as TabCause;
 pub use una_op::Cause as UnaOpCause;
 
@@ -60,7 +62,6 @@ impl Error {
             BinOp(cause) => {
                 if let OpCode::BinOp(op) = opcode {
                     let debug_info = debug_info.and_then(Extract::extract);
-
                     cause.into_diagnostic(file_id, debug_info, op)
                 } else {
                     malformed_diagnostic()
@@ -97,50 +98,6 @@ impl From<BinOpCause> for Cause {
 impl From<UnaOpCause> for Cause {
     fn from(value: UnaOpCause) -> Self {
         Cause::UnaOp(value)
-    }
-}
-
-#[derive(Debug, Clone)]
-pub struct MissingArgsError {
-    pub expected_args: usize,
-    pub stack_len: usize,
-}
-
-impl MissingArgsError {
-    fn into_diagnostic<FileId>(
-        self,
-        file_id: FileId,
-        opcode: OpCode,
-        debug_info: Option<Range<usize>>,
-    ) -> Diagnostic<FileId> {
-        let MissingArgsError {
-            expected_args,
-            stack_len,
-        } = self;
-
-        let name = opcode.name();
-        let have_debug_info = debug_info.is_some();
-        let mut diag = Diagnostic::bug()
-            .with_message(format!("bytecode instruction {name} expects {expected_args} argument(s) but only {stack_len} were provided"));
-
-        if let Some(span) = debug_info {
-            diag.with_label([Label::secondary(file_id, span)
-                .with_message("error occurred here, but its cause in different place")]);
-        }
-
-        diag.with_note(compiler_bug([
-            "the opcode gets its arguments from stack but it seems there is not enough values to take",
-            "the error implies that too few values were put on stack however it could have happenned long before this point",
-        ]));
-
-        if !have_debug_info {
-            diag.with_note([
-                "no debug info is available",
-                "it is possible debug info was stripped\nit is also possible that erroneous bytecode was handcrafted\nplease check with where you got it",
-            ]);
-        }
-
-        diag
     }
 }
 
