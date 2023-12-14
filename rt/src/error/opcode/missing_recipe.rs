ffe4c23709007f150595d242c727f30dc7673902
haibane_tenshi <obscure.sanctum@gmail.com>
2023-12-14 05:32:51 +0300

    feat(rt::error::opcode:MissingRecipe): improve diagnostic message

--- rt/src/error/opcode/missing_recipe.rs
+++ rt/src/error/opcode/missing_recipe.rs
@@ -12,43 +12,45 @@ impl MissingRecipe {
         file_id: FileId,
         opcode: OpCode,
         debug_info: Option<OpCodeDebugInfo>,
-    ) -> Diagnostic<FileId> {
+    ) -> Diagnostic<FileId>
+    where
+        FileId: Clone,
+    {
         use super::ExtraDiagnostic;
         use codespan_reporting::diagnostic::Label;
 
-        let MissingRecipe(const_id) = self;
+        let MissingRecipe(recipe_id) = self;
         let have_debug_info = debug_info.is_some();
 
         let name = opcode.name();
         let mut diag = Diagnostic::bug().with_message(format!(
-            "bytecode instruction {name} attempted to load non-existent closure recipe with id={const_id}",
+            "bytecode instruction {name} attempted to load non-existent closure recipe with id={recipe_id}",
         ));
 
-        // if let Some(debug_info) = debug_info {
-        //     match (opcode, debug_info) {
-        //         (OpCode::LoadConstant(_), OpCodeDebugInfo::LoadConst(info)) => diag
-        //             .with_label([Label::primary(file_id, info.literal)
-        //                 .with_message("constructed out of this literal")]),
-        //         (OpCode::LoadConstant(_), OpCodeDebugInfo::Generic(span)) => diag.with_label([
-        //             Label::secondary(file_id, span).with_message("triggered by this"),
-        //         ]),
-        //         _ => (),
-        //     }
-        // }
+        if let Some(debug_info) = debug_info {
+            match (opcode, debug_info) {
+                (OpCode::MakeClosure(_), OpCodeDebugInfo::MakeClosure(info)) => diag.with_label([
+                    Label::primary(file_id.clone(), info.function_token),
+                    Label::secondary(file_id, info.total_span)
+                        .with_message("function constructor here"),
+                ]),
+                _ => (),
+            }
+        }
 
-        // match opcode {
-        //     OpCode::LoadConstant(id) if id == const_id => diag.with_compiler_bug_note([
-        //         "the opcode loads constant from constant table of the chunk current function is defined in",
-        //     ]),
-        //     OpCode::LoadConstant(id) => diag.with_runtime_bug_note([
-        //         format!("the opcode should load constant with id={id}, but it attempted to load constant with id={const_id}"),
-        //         "likely, this diagnostic is malformed".to_string(),
-        //     ]),
-        //     _ => diag.with_runtime_bug_note([
-        //         format!("instruction {name} doesn't access constants"),
-        //         "likely, this diagnostic is malformed".to_string(),
-        //     ])
-        // };
+        match opcode {
+            OpCode::MakeClosure(id) if id == recipe_id => diag.with_compiler_bug_note([
+                "the opcode loads closure recipe from the chunk current function is defined in",
+            ]),
+            OpCode::MakeClosure(id) => diag.with_runtime_bug_note([
+                format!("the opcode should load closure recipe with id={id}, but it attempted to load recipe with id={recipe_id}"),
+                "likely, this diagnostic is malformed".to_string(),
+            ]),
+            _ => diag.with_runtime_bug_note([
+                format!("instruction {name} doesn't access closure recipes"),
+                "likely, this diagnostic is malformed".to_string(),
+            ])
+        };
 
         if !have_debug_info {
             diag.no_debug_info();
