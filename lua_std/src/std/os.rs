a1b6a1e3c5de84e595ecaa0eda81b7fc76a49915
haibane_tenshi <obscure.sanctum@gmail.com>
2025-02-23 11:38:03 +0300

    feat(lua_std::std::os::setlocale): add API

--- lua_std/src/std/os.rs
+++ lua_std/src/std/os.rs
@@ -325,3 +325,42 @@ where
         set_func("rename", fn_body).build(table, core);
     }
 }
+
+/// Set current locale.
+///
+/// # From Lua documentation
+///
+/// **Signature:**
+/// * `(locale: nil | string, [category: string]) -> string | fail`
+///
+/// Sets the current locale of the program.
+/// `locale` is a system-dependent string specifying a locale;
+/// category is an optional string describing which category to change: "all", "collate", "ctype", "monetary", "numeric", or "time";
+/// the default category is "all".
+/// The function returns the name of the new locale, or **fail** if the request cannot be honored.
+///
+/// If `locale` is the empty string, the current locale is set to an implementation-defined native locale.
+/// If `locale` is the string "C", the current locale is set to the standard C locale.
+///
+/// When called with `nil` as the first argument, this function only returns the name of the current locale for the given category.
+///
+/// This function may be not thread safe because of its reliance on C function `setlocale`.
+///
+/// # Implementation-specific behavior
+///
+/// *   Our implementation of Lua std is independent from C locale, therefore this function does nothing (besides validating arguments).
+///     It will always return **fail**.
+#[expect(non_camel_case_types)]
+pub struct setlocale;
+
+impl<Ty> TableEntry<Ty> for setlocale
+where
+    Ty: Types<RustClosure = Box<dyn DLuaFfi<Ty>>>,
+    PathBuf: ParseFrom<Ty::String>,
+    <PathBuf as ParseFrom<Ty::String>>::Error: Display,
+{
+    fn build(self, table: &RootTable<Ty>, core: &mut Core<Ty>) {
+        let fn_body = ffi::from_fn(crate::ffi::os::setlocale, "lua_std::std::os::setlocale", ());
+        set_func("setlocale", fn_body).build(table, core);
+    }
+}
