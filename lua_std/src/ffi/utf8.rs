8f9e87a76edceb6965e7c22f4d6a41fb7b99105b
haibane_tenshi <obscure.sanctum@gmail.com>
2025-02-25 10:37:00 +0300

    feat(lua_std::ffi::utf8::codes): add API

--- lua_std/src/ffi/utf8.rs
+++ lua_std/src/ffi/utf8.rs
@@ -18,16 +18,127 @@
 //! By default, they only accept byte sequences that result in valid Unicode code points, rejecting values greater than `10FFFF` and surrogates.
 //! A boolean argument `lax`, when available, lifts these checks, so that all values up to `0x7FFFFFFF` are accepted.
 //! (Not well formed and overlong sequences are still rejected.)
+//!
+//! # Known incompatibilities
+//!
+//! *   There is no support for encoding/decoding integer values which do not represent valid Unicode code points.
+//!
+//!     While it is true that original UTF-8 spec permitted encoding values in range`0..=0x7FFFFFFF`,
+//!     IETF [RFC 3629][rfc#3629] that restricted range of available code points only to 4 byte sequences
+//!     and removed range of surrogate code points was **published in 2003**.
+//!
+//!     At this point UTF-8 is universally understood and defined as *Unicode encoding form*.
+//!     To clarify, this implies two assumptions:
+//!
+//!     * It defines a mapping between a binary payload (of up to 21 bits) and an octet stream;
+//!     * Binary payload represents a valid Unicode code point.
+//!
+//!     Here, Lua basically considers the last point *optional*, which is meaningless.
+//!     According to the standard there is no such thing as valid UTF-8 encoded sequence that does not contain Unicode code points.
+//!     We consider this a **legacy behavior** and current implementation opts out of supporting it.
+//!
+//!     It is obviously still possible to encode/decode arbitrary 31-bit values to/from octets using UTF-8 scheme (resulting in up to 6-byte sequences),
+//!     however it should not be labeled as an actual UTF-8 encoding to avoid confusion.
+//!     Instead it should be considered a distinct variable-length encoding.
+//!     This functionality can be provided by a different library if needed.
+//!
+//! [rfc#3629]: https://datatracker.ietf.org/doc/html/rfc3629
 
 use rt::ffi::delegate::{self, Delegate};
+use rt::ffi::DLuaFfi;
 use rt::value::Types;
+
+enum Error {
+    MissingBytes,
+    InvalidLeadingByte,
+    Overlong,
+}
+
+/// Attempt to extract next utf-8 encoded code point.
+///
+/// Overlong sequences (encoding values into more bytes than necessary) will be rejected.
+///
+/// # Returns
+///
+/// Second return indicates number of bytes consumed.
+fn next_code_point(data: &[u8]) -> Result<(u32, usize), Error> {
+    assert!(!data.is_empty());
+
+    const TRAIL_MASK: u8 = 0b00111111;
+
+    let byte0 = data[0];
+    match byte0.leading_ones() {
+        0 => {
+            let part0 = (byte0 & 0b0111111) as u32;
+
+            Ok((part0, 1))
+        }
+        2 => {
+            let [_, byte1, ..] = data else {
+                return Err(Error::MissingBytes);
+            };
+
+            let part0 = (byte0 & 0b00011111) as u32;
+            let part1 = (byte1 & TRAIL_MASK) as u32;
+            let output = (part0 << 6) | part1;
+
+            if (0x80..0x800).contains(&output) {
+                Ok((output, 2))
+            } else {
+                Err(Error::Overlong)
+            }
+        }
+        3 => {
+            let [_, byte1, byte2, ..] = data else {
+                return Err(Error::MissingBytes);
+            };
+
+            let part0 = (byte0 & 0b00001111) as u32;
+            let part1 = (byte1 & TRAIL_MASK) as u32;
+            let part2 = (byte2 & TRAIL_MASK) as u32;
+            let output = (part0 << 12) | (part1 << 6) | part2;
+
+            if (0x800..0x10000).contains(&output) {
+                Ok((output, 3))
+            } else {
+                Err(Error::Overlong)
+            }
+        }
+        4 => {
+            let [_, byte1, byte2, byte3, ..] = data else {
+                return Err(Error::MissingBytes);
+            };
+
+            let part0 = (byte0 & 0b00000111) as u32;
+            let part1 = (byte1 & TRAIL_MASK) as u32;
+            let part2 = (byte2 & TRAIL_MASK) as u32;
+            let part3 = (byte3 & TRAIL_MASK) as u32;
+            let output = (part0 << 18) | (part1 << 12) | (part2 << 6) | part3;
+
+            if (0x10000..0x200000).contains(&output) {
+                Ok((output, 4))
+            } else {
+                Err(Error::Overlong)
+            }
+        }
+        _ => Err(Error::InvalidLeadingByte),
+    }
+}
 
 /// Encode sequence of integers with UTF-32 codes into a string.
 ///
 /// # From Lua documentation
 ///
+/// **Signature:**
+/// * `(...: int) -> string`
+///
 /// Receives zero or more integers, converts each one to its corresponding UTF-8 byte sequence and
 /// returns a string with the concatenation of all these sequences.
+///
+/// # Implementation-specific behavior
+///
+/// *   Integers must represent valid Unicode code points.
+///     See module-level notes on [known incompatibilities](self#known-incompatibilities).
 pub fn char<Ty>() -> impl Delegate<Ty>
 where
     Ty: Types,
@@ -74,6 +185,129 @@ where
         rt.stack.transient_in(&mut rt.core.gc, |mut stack, heap| {
             stack.clear();
             stack.push(heap.alloc_str(r).downgrade());
+        });
+
+        Ok(())
+    })
+}
+
+/// Iterate over Unicode code points in a string.
+///
+/// From Lua documentation
+///
+/// **Signature:**
+/// * `(s: string, [lax: bool]) -> (function, string, int)`
+///
+/// Returns values so that the construction
+///
+/// ```lua
+/// for p, c in utf8.codes(s) do body end
+/// ```
+///
+/// will iterate over all UTF-8 characters in string `s`, with `p` being the position (in bytes) and `c` the code point of each character.
+/// It raises an error if it meets any invalid byte sequence.
+///
+/// # Notes
+///
+/// * Remember that Lua indexing starts with 1 and not 0.
+///
+/// # Implementation-specific behavior
+///
+/// *   `lax` mode is ignored.
+///     See module-level notes on [known incompatibilities](self#known-incompatibilities).
+///
+/// *   It is not required for entire string to be valid utf8,
+///     iterator will decode characters one-by-one in order.
+///     However encountering invalid byte sequences will result in Lua panic.
+pub fn codes<Ty>() -> impl Delegate<Ty>
+where
+    Ty: Types<RustClosure = Box<dyn DLuaFfi<Ty>>>,
+{
+    fn iter<Ty>() -> impl Delegate<Ty>
+    where
+        Ty: Types,
+    {
+        delegate::from_mut(|mut rt| {
+            use rt::ffi::arg_parser::{Int, LuaString, ParseArgs};
+            use rt::gc::AllocExt;
+            use rt::value::Value;
+
+            let (s, original_pos): (LuaString<_>, Int) = rt.stack.parse(&mut rt.core.gc)?;
+            rt.stack.clear();
+            let data = s.to_bytes(&rt.core.gc)?;
+            let original_pos = original_pos.0;
+            let pos = original_pos
+                .checked_sub(1)
+                .and_then(|pos| pos.try_into().ok());
+
+            let pos = match pos {
+                Some(pos) if (0..=data.len()).contains(&pos) => pos,
+                _ => {
+                    let err = rt
+                        .core
+                        .gc
+                        .alloc_error_msg(format!("index {original_pos} out of bounds"));
+                    return Err(err);
+                }
+            };
+
+            let tail = &data[pos..];
+            if tail.is_empty() {
+                rt.stack.transient().push(Value::Nil);
+                return Ok(());
+            }
+
+            let (code_point, offset) = match next_code_point(tail) {
+                Ok(t) => t,
+                Err(Error::InvalidLeadingByte | Error::MissingBytes) => {
+                    let err = rt.core.gc.alloc_error_msg(format!(
+                        "byte sequence at {original_pos} does not denote valid utf8 sequence"
+                    ));
+                    return Err(err);
+                }
+                Err(Error::Overlong) => {
+                    let err = rt.core.gc.alloc_error_msg(format!(
+                        "byte sequence at {original_pos} contains an overlong utf8 sequence"
+                    ));
+                    return Err(err);
+                }
+            };
+
+            if char::from_u32(code_point).is_none() {
+                let err = rt.core.gc.alloc_error_msg(format!(
+                    "utf8 sequence at {original_pos} does not contain a valid unicode code point"
+                ));
+                return Err(err);
+            }
+
+            // Need +1 because Lua indexing start with 1 and not 0.
+            let pos = pos + offset + 1;
+
+            rt.stack.transient_in(&mut rt.core.gc, |mut stack, _| {
+                stack.push(Value::Int(pos.try_into().unwrap()));
+                stack.push(Value::Int(code_point.into()));
+            });
+
+            Ok(())
+        })
+    }
+
+    delegate::from_mut(|mut rt| {
+        use rt::ffi::arg_parser::{Boolean, LuaString, Opts, ParseArgs, Split};
+        use rt::ffi::{boxed, from_fn};
+        use rt::gc::LuaPtr;
+        use rt::value::{Callable, Value};
+
+        let (s, rest): (LuaString<_>, Opts<(Boolean,)>) = rt.stack.parse(&mut rt.core.gc)?;
+        let (_lax,) = rest.split();
+
+        rt.stack.transient_in(&mut rt.core.gc, |mut stack, heap| {
+            let callable = from_fn(iter, "lua_std::std::codes::iter", ());
+            let callable = Callable::Rust(LuaPtr(heap.alloc_cell(boxed(callable)))).downgrade();
+
+            stack.push(callable.into());
+            stack.push(s.into());
+            stack.push(Value::Int(1));
         });
 
         Ok(())
